5f5d3c0d97ba98be733ba6f02d4141720c31e0d6
Yussef Dalton <r0930164@student.thomasmore.be>
2025-01-01 15:28:00 +0100

    Add phone number

--- PcParts.Bot/CoreBot/DialogDetails/OrderDetails.cs
+++ PcParts.Bot/CoreBot/DialogDetails/OrderDetails.cs
@@ -8,6 +8,7 @@ namespace CoreBot.DialogDetails
         public string Name { get; set; }
         public bool IsDelivery { get; set; }
         public string EmailAddress { get; set; }
+        public string PhoneNumber { get; set; }
         public string StreetAddress { get; set; }
         public string City { get; set; }
         public string ZipCode { get; set; }
